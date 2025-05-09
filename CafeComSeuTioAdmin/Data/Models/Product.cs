3f5eb58939537b4529bbc411db85a8533961b4b1
Richard-Alves167 <riichardd.08@gmail.com>
2025-05-09 11:11:17 -0300

    Adicionado método para listar os produtos salvos no CSV na tabela da página de ViewAllProducts.

--- CafeComSeuTioAdmin/Data/Models/Product.cs
+++ CafeComSeuTioAdmin/Data/Models/Product.cs
@@ -5,6 +5,33 @@ namespace CafeComSeuTioAdmin.Data
 {
     public class Product
     {
+        public Product() { }
+        public Product(string nome, string descricaoPequena, string descricao, decimal preco, string categoria) {
+            Name = nome;
+            ShortDescription = descricaoPequena;
+            Description = descricao;
+            Price = preco;
+            Category = categoria;
+        }
+        public List<Product> ListaProdutos() {
+            var filename = $"Produtos.txt";
+            var caminho = Path.Combine("wwwroot/files", filename);
+            List<Product> listaProdutos = new List<Product>();
+            try {
+                using (StreamReader arquivo = new StreamReader(caminho)) {
+                    string linha = arquivo.ReadLine();
+                    while (linha != null) {
+                        string[] informacoesProduto = linha.Split(",");
+                        Product product = new Product(informacoesProduto[0], informacoesProduto[1], informacoesProduto[2], decimal.Parse(informacoesProduto[3]), informacoesProduto[4]);
+                        listaProdutos.Add(product);
+                        linha = arquivo.ReadLine();
+                    }
+                }
+            } catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+            return listaProdutos;
+        }
         public int Id { get; set; }
 
         [Required]
--- CafeComSeuTioAdmin/Pages/Products/ViewAllProducts.cshtml.cs
+++ CafeComSeuTioAdmin/Pages/Products/ViewAllProducts.cshtml.cs
@@ -6,12 +6,15 @@ namespace CafeComSeuTioAdmin.Pages.Products
 {
     public class ViewAllProductsModel : PageModel
     {
+        public List<Product> ListaProduto;
         public string NomeProduto { get; set; }
         public double PrecoProduto { get; set; }
         public int QuantidadeProduto { get; set; }
         public string DescricaoProduto { get; set; }
         public void OnGet()
         {
+            Product produto = new Product();
+            ListaProduto = produto.ListaProdutos();
         }
 
         public void OnPost() {
