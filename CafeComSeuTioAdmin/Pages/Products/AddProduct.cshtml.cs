885e7a3bca489452d52317ca9586c7de319a754f
Richard-Alves167 <riichardd.08@gmail.com>
2025-05-09 11:05:01 -0300

    Adicionado no OnPost de AddProduct, método para salvar as informações de um produto em um arquivo CSV.

--- CafeComSeuTioAdmin/Pages/Products/AddProduct.cshtml.cs
+++ CafeComSeuTioAdmin/Pages/Products/AddProduct.cshtml.cs
@@ -13,6 +13,23 @@ namespace CafeComSeuTioAdmin.Pages.Products
 
         public void OnPost() {
             if (ModelState.IsValid) {
+                var filename = $"Produtos.txt";
+                var caminho = Path.Combine("wwwroot/files", filename);
+
+                var productName = newProduct.Name;
+                var productShortDescription = newProduct.ShortDescription;
+                var productDescription = newProduct.Description;
+                var productPrice = newProduct.Price;
+                var productCategory = newProduct.Category;
+
+                using (StreamWriter escrever = new StreamWriter(caminho, true)) {
+                    escrever.WriteLine($"{productName},{productShortDescription},{productDescription},{productPrice},{productCategory}");
+                    //escrever.WriteLine("Nome do Produto: " + productName);
+                    //escrever.WriteLine("Descrição pequena do Produto: " + productShortDescription);
+                    //escrever.WriteLine("Descrição do Produto: " + productDescription);
+                    //escrever.WriteLine("Preço do Produto: " + productPrice);
+                    //escrever.WriteLine("Categoria do Produto: " + productCategory);
+                }
             }
         }
     }
