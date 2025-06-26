d44b4eae3e8a0655bd3b691edff73b179a62add8
Richard-Alves167 <riichardd.08@gmail.com>
2025-06-25 22:55:09 -0300

    Implementation: implementando a página ProductCategory com o arquivo css, a rota e retorno de produtos definidos pela categoria; com a implementação de método de retornar produtos específicos pela categoria nos repositórios.

---
+++ CafeComSeuTioAdmin/Pages/ProductCategory.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CafeteriaKwai.Pages
+{
+    public class ProductCategoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
