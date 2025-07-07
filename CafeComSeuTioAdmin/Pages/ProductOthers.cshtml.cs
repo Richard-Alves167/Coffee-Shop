43f1617e18ea1506a3429d8cdc1453bada66de18
Richard-Alves167 <riichardd.08@gmail.com>
2025-07-07 10:45:53 -0300

    FEAT: implementando a página 'ProductOthers' e referenciando essa página na navegação de topo em 'Outros' e na página 'Index' na sessão 'Nosso Cardápio' no card 'Outros'.

---
+++ CafeComSeuTioAdmin/Pages/ProductOthers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CafeteriaKwai.Pages
+{
+    public class ProductOthersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
