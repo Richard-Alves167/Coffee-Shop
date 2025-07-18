f10cb7112e9a6fd1eb762a29b43a8fe011f97a22
Richard-Alves167 <riichardd.08@gmail.com>
2025-07-18 10:52:32 -0300

    FEAT: implementado uma página para ver as informações de sua conta de usuário.

---
+++ CafeComSeuTioAdmin/Pages/Identity/Pages/Account/AccountInfo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CafeteriaKwai.Pages.Identity.Pages.Account
+{
+    public class AccountInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- CafeComSeuTioAdmin/Pages/Identity/Pages/Account/Register.cshtml.cs
+++ CafeComSeuTioAdmin/Pages/Identity/Pages/Account/Register.cshtml.cs
@@ -75,6 +75,15 @@ namespace CafeteriaKwai.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
+            [StringLength(16, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [Display(Name = "Name")]
+            public string Name { get; set; }
+
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -114,9 +123,15 @@ namespace CafeteriaKwai.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.Name, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
+
+                var existingUser = await _userManager.FindByNameAsync(Input.Name);
+                //if (existingUser != null) {
+                //    ModelState.AddModelError("Input.Name", "Username already in use.");
+                //    return Page();
+                //}
 
                 if (result.Succeeded)
                 {
