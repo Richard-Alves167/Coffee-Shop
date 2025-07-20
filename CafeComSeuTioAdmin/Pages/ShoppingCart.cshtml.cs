c549a61bce9250faa7d00843b03b77b6dad3af81
Richard-Alves167 <riichardd.08@gmail.com>
2025-07-20 18:42:07 -0300

    FEAT: implementado a página de 'ShoppingCart' para visualizar e modificar seus produtos antes de ser efetuado um pedido.

---
+++ CafeComSeuTioAdmin/Pages/ShoppingCart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CafeteriaKwai.Pages
+{
+    public class ShoppingCartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
