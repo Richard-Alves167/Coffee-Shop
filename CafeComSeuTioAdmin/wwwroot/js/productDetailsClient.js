503b4e4362e649a329ac423decf5c88666f5abf2
Richard de Jesus Cabral Alves <richard.alves@al.infnet.edu.br>
2025-08-04 10:30:36 -0300

    FEAT: adicionado comandos para a seleção e mudança de imagem das estrelas da avaliação, para a seção de editar a avaliação na página 'ProductDetailsClient'.

--- CafeComSeuTioAdmin/wwwroot/js/productDetailsClient.js
+++ CafeComSeuTioAdmin/wwwroot/js/productDetailsClient.js
@@ -9,3 +9,15 @@
         starSelected.setAttribute("src", "/images/iconsAction/estrelaCheia.png");
     }
 }
+function EditStarAvaliation(input) {
+    for (i = 1; i <= 5; i++) {
+        var starSelected = document.getElementById("editStarImgValue" + i);
+        starSelected.setAttribute("src", "/images/iconsAction/estrelaVazia.png");
+    }
+    var starValue = input.value;
+    for (i = 1; i <= starValue; i++) {
+        var starSelected = document.getElementById("editStarImgValue" + i);
+        starSelected.setAttribute("src", "/images/iconsAction/estrelaCheia.png");
+    }
+}
+
