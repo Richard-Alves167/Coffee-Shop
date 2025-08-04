a3df4200c07e4ff133f48b1ffcfde6f27c660392
Richard de Jesus Cabral Alves <richard.alves@al.infnet.edu.br>
2025-08-04 12:54:58 -0300

    STYLE-FEAT: adicionado javascript e css para uma tela de carregamento, e implementado ao mudar de seções na página 'AccountInfo'.

---
+++ CafeComSeuTioAdmin/wwwroot/js/loadContainer.js
@@ -0,0 +1,13 @@
+﻿function Loading() {
+    let loadDiv = document.createElement("div");
+    loadDiv.setAttribute("id", "loadContainer");
+    let loadImage = document.createElement("img");
+    loadImage.setAttribute("src", "/images/cats/runningCat.gif");
+    let loadSpan = document.createElement("span");
+    loadSpan.textContent = "Loading . . .";
+    loadDiv.appendChild(loadImage);
+    loadDiv.appendChild(loadSpan);
+    document.body.appendChild(loadDiv);
+
+    setTimeout(() => document.body.removeChild(loadDiv), 2500);
+}
