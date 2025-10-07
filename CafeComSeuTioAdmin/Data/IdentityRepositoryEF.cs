c69a11c1f791578bb25343b0ff357fbaa7821c98
Richard-Alves167 <riichardd.08@gmail.com>
2025-10-06 23:42:44 -0300

    FEAT: implementação de um repositório para a classe de identidade com Entity Framework

---
+++ CafeComSeuTioAdmin/Data/IdentityRepositoryEF.cs
@@ -0,0 +1,32 @@
+﻿using CafeteriaKwai.Data.Models;
+
+namespace CafeteriaKwai.Data {
+    public class IdentityRepositoryEF : IIdentityRepository {
+        ApplicationDbContext identidadeContext {  get; set; }
+        public IdentityRepositoryEF(ApplicationDbContext context) {
+            identidadeContext = context;
+        }
+        public void Add(ApplicationUser user) {
+            identidadeContext.Users.Add(user);
+            identidadeContext.SaveChanges();
+        }
+        public List<ApplicationUser> GetAll() {
+            return identidadeContext.Users.ToList();
+        }
+        public ApplicationUser GetById(string id) {
+            return identidadeContext.Users.FirstOrDefault(u => u.Id == id);
+        }
+        public ApplicationUser GetByEmail(string email) {
+            return identidadeContext.Users.Where(i => i.Email == email).FirstOrDefault();
+        }
+        public void Update(ApplicationUser user) {
+            identidadeContext.Users.Update(user);
+            identidadeContext.SaveChanges();
+        }
+        public void Delete(string id) {
+            var DeletedUser = identidadeContext.Users.Where(i => i.Id == id).FirstOrDefault();
+            identidadeContext.Users.Remove(DeletedUser);
+            identidadeContext.SaveChanges();
+        }
+    }
+}
--- CafeComSeuTioAdmin/Pages/Identity/Pages/Account/AccountInfo.cshtml.cs
+++ CafeComSeuTioAdmin/Pages/Identity/Pages/Account/AccountInfo.cshtml.cs
@@ -1,11 +1,21 @@
+using CafeteriaKwai.Data;
+using CafeteriaKwai.Data.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CafeteriaKwai.Pages.Identity.Pages.Account
 {
     public class AccountInfoModel : PageModel
     {
-        public void OnGet()
-        {
+        public IIdentityRepository identityRepository;
+
+        public AccountInfoModel(IIdentityRepository identityRepository, IWebHostEnvironment webEnv) {
+            this.identityRepository = identityRepository;
+        }
+
+        [BindProperty]
+        public ApplicationUser user { get; set; }
+        public void OnGet() {
         }
     }
 }
